d6964ae8464c312c9c3071ebfe332ec1c8ba8362
behrad <mbehrad1432@gmail.com>
2026-01-08 05:11:51 +0330

    chore: register RewardReport model in Django admin

--- backend/investigation/admin.py
+++ backend/investigation/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import RewardReport
+
+
+@admin.register(RewardReport)
+class RewardReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'reporter', 'suspect_full_name', 'reward_amount', 'tracking_code', 'is_paid')
+    list_filter = ('status', 'is_paid')
+    search_fields = ('suspect_full_name', 'suspect_national_code', 'tracking_code')
+
